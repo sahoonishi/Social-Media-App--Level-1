fa9ef1f0d6c209040759bd8db15d7d6c7833448c
sahoonishi <nishisahoo293@gmail.com>
2024-06-05 22:47:30 +0530

    spinner

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const LoadingSpinner = () => {
   return (
-<div className="d-flex justify-content-center loading">
-  <div className="spinner-border" role="status">
-    <span className="visually-hidden">Loading...</span>
-  </div>
-</div>
-  )
-}
+    <div className="d-flex justify-content-center loading">
+      <div className="spinner-border" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </div>
+    </div>
+  );
+};
 
-export default LoadingSpinner
+export default LoadingSpinner;
